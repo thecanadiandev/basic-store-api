0510f9a103a285eb070470a305ca66bd97304a86
Hari <harikrishkk@gmail.com>
2024-10-11 14:22:14 -0400

    add sorting

--- controllers/products.js
+++ controllers/products.js
@@ -3,14 +3,15 @@ const Product = require('../models/product')
 const getAllProductsStatic = async (req, res) => {
   //const products = await Product.find({ featured: true })
   const search = req.query.name
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' }
-  })
+  // const products = await Product.find({
+  //   name: { $regex: search, $options: 'i' }
+  // })
+  const products = await Product.find({}).sort('-name price')
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
 
   const queryObject = {};
   if (featured) {
@@ -22,7 +23,19 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' }
   }
-  const products = await Product.find(queryObject)
+  // dont call await here. 
+  let results = Product.find(queryObject);
+
+  // since we need to chain the sort,
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    results = results.sort(sortList);
+  } else {
+    results = results.sort('createdAt');
+  }
+
+  // once query is ready, call await
+  const products = await results;
   res.status(200).json({ products, nbHits: products.length })
 }
 
