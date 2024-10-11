a90487ac8d55b43f9026ba695f21a8cd7220fded
Hari <harikrishkk@gmail.com>
2024-10-11 13:48:51 -0400

    add query object

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,26 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ featured: true })
+  //const products = await Product.find({ featured: true })
+  const search = req.query.name
+  const products = await Product.find({
+    name: { $regex: search, $options: 'i' }
+  })
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name } = req.query;
 
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }
   }
   const products = await Product.find(queryObject)
   res.status(200).json({ products, nbHits: products.length })
