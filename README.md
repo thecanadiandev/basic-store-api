875f94c1b5bc9907a2c71d1736d4a7cfff54e4b1
Hari <harikrishkk@gmail.com>
2024-10-11 14:51:24 -0400

    add filters

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - express async errors is a middleware package that helps mangage errors.
 - Instead of having a next, we just need to throw the error.
+- This repo is about playing around with product controllers, and learning search, sort, filter, limit responses.
--- controllers/products.js
+++ controllers/products.js
@@ -6,14 +6,15 @@ const getAllProductsStatic = async (req, res) => {
   // const products = await Product.find({
   //   name: { $regex: search, $options: 'i' }
   // })
-  const products = await Product.find({}).sort('-name price')
+  const products = await Product.find({ price: { $gt: 30 } }).sort('-name price').limit(10)
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
 
   const queryObject = {};
+
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false
   }
@@ -22,6 +23,33 @@ const getAllProducts = async (req, res) => {
   }
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' }
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ['price', 'rating']
+
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+
+      if (options.includes(field)) {
+        queryObject[field] = {
+          [operator]: Number(value),
+        }
+      }
+    })
   }
   // dont call await here. 
   let results = Product.find(queryObject);
@@ -33,6 +61,16 @@ const getAllProducts = async (req, res) => {
   } else {
     results = results.sort('createdAt');
   }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    results = results.select(fieldsList);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  results = results.skip(skip).limit(limit);
 
   // once query is ready, call await
   const products = await results;
